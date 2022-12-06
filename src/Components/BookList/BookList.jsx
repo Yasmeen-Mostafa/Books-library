b722f0cbf8216b48fdd34bd7b60113a9360b7c75
Yasmeen-Mostafa <yasmeen.mostafa1236@gmail.com>
2022-12-06 14:39:17 +0200

    Delete book added

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,5 +1,9 @@
+import BookShow from "../BookShow/BookShow";
 import "./BookList.scss";
-const BookList = () => {
-  return <div></div>;
+const BookList = ({ books, onDelete }) => {
+  const renderedBooks = books.map((book) => {
+    return <BookShow onBookDelete={onDelete} book={book} key={book.id} />;
+  });
+  return <div className="book__list">{renderedBooks}</div>;
 };
 export default BookList;
--- src/Components/BookShow/BookShow.jsx
+++ src/Components/BookShow/BookShow.jsx
@@ -1,5 +1,17 @@
 import "./BookShow.scss";
-const BookShow = () => {
-  return <div></div>;
+const BookShow = ({ book, onBookDelete }) => {
+  //book is the object that hold id of book and title
+  const onDeleteClick = () => {
+    onBookDelete(book.id);
+  };
+  return (
+    <div className="book__show">
+      <h3>{book.title}</h3>
+      <button onClick={onDeleteClick} className="delete">
+        X
+      </button>
+      <button className="edit">📝</button>
+    </div>
+  );
 };
 export default BookShow;
